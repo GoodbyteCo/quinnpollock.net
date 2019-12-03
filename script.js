3985c2e986c356ebc9c71554d6ff0caaa6194c5d
Jack Guinane <jackguinane@gmail.com>
2019-12-02 21:37:29 -0500

    normalize formatting

--- script.js
+++ script.js
@@ -3,26 +3,23 @@ const isLightMode = window.matchMedia("(prefers-color-scheme: light)").matches;
 const isNotSpecified = window.matchMedia("(prefers-color-scheme: no-preference)").matches;
 const hasNoSupport = !isDarkMode && !isLightMode && !isNotSpecified;
 
-
-
-function darkMode(btn){
+function darkMode(btn) {
     btn.innerHTML = "dark mode: on";
     document.body.className = "dark-mode";	
 }
 
-function lightMode(btn){
+function lightMode(btn) {
     btn.innerHTML = "dark mode: off";
     document.body.className = "";
-
 }
 
-function defaultMode(){
-    if(!(CSS.supports("backdrop-filter: invert(1)") || CSS.supports("-webkit-backdrop-filter: invert(1)"))){
+function defaultMode() {
+    if(!(CSS.supports("backdrop-filter: invert(1)") || CSS.supports("-webkit-backdrop-filter: invert(1)"))) {
         console.log("firefox")
         var btn = document.getElementById("dark-mode-btn");
         btn.innerHTML = "" 
     } else {
-        if(isDarkMode){
+        if(isDarkMode) {
             var btn = document.getElementById("dark-mode-btn");
             darkMode(btn)
         }
@@ -33,25 +30,23 @@ window.matchMedia("(prefers-color-scheme: dark)").addListener(e => {
     var btn = document.getElementById("dark-mode-btn");
     e.matches && darkMode(btn)
     });
+
 window.matchMedia("(prefers-color-scheme: light)").addListener(e => {
     var btn = document.getElementById("dark-mode-btn"); 
         e.matches && lightMode(btn)
-        });
+    });
 
-function changeMode()
-{
+function changeMode() {
     document.getElementById("nav").classList.toggle("closed");
     var btn = document.getElementById("dark-mode-btn");
     
-    if(btn.innerHTML == "dark mode: off")
-    {
+    if(btn.innerHTML == "dark mode: off") {
         darkMode(btn)
-    }
-    else
-    {
+    } else {
         lightMode(btn)
     }
 }
+
 window.addEventListener('DOMContentLoaded', (event) => {
     defaultMode();
 });
